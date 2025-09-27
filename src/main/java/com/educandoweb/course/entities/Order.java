8eae49dd0544aebd55517f607c992f1b272f270b
Lucas <lucas.iferreir@gmail.com>
2025-09-27 17:52:26 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -95,6 +95,13 @@ public class Order implements Serializable {
     public void setPayment(Payment payment) {
         this.payment = payment;
     }
+    public Double getTotal(){
+        double som=0.0;
+        for(OrderItem o:items){
+            som+=o.getSubtotal();
+        }
+        return som;
+    }
 
     @Override
     public boolean equals(Object o) {
